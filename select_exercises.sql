910fec3fbb4a0ba7578d98e5f3cdaef0e84f9666
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-03-28 14:53:23 -0500

    adds select exercises

---
+++ select_exercises.sql
@@ -0,0 +1,32 @@
+USE codeup_test_db;
+
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd'
+
+
+SELECT release_date
+FROM albums
+Where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+
+SELECT genre
+FROM albums
+Where name = 'Nevermind'
+
+
+
+SELECT name
+FROM albums
+Where release_date = BETWEEN 1989 AND 2000
+
+
+SELECT name
+FROM albums
+Where sales < 20.0
+
+
+SELECT name
+FROM albums
+Where genre = 'Rock'
+
