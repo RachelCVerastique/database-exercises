e2777256e42dc855f13f6fe4bec497bedd043155
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-03-28 15:59:29 -0500

    adds delete exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,1 +1,18 @@
 USE codeup_test_db;
+
+-- #4a
+-- SELECT name
+DELETE FROM albums
+WHERE release_date > 1991
+
+
+-- #4b
+-- SELECT name
+DELETE FROM albums
+WHERE genre = 'disco'
+
+
+-- 4c
+-- SELECT name
+DELETE FROM albums
+WHERE artist = 'Whitney Houston'
